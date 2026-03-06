552a537a86e9e1cb75796258fd9545426dcfd908
harrylee <22520435@gm.uit.edu.vn>
2026-03-07 00:21:22 +0700

    Update solution for transformers-positional-encoding

--- transformer/transformers-positional-encoding/transformers-positional-encoding.py
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -7,12 +7,11 @@ def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
 
     PE = []
     for idx in range(seq_length):
-        for _dim in range(0, d_model, 2):
-            PE.append([np.sin(idx/(10000**(idx/d_model))),np.cos(idx/(10000**(idx/d_model)))])
-        
-
+        for dim in range(0, d_model, 2):
+            PE.append([np.sin(idx/(10000**((dim)/d_model))), np.cos(idx/(10000**((dim)/d_model)))])
         
     PE = np.reshape(PE,(seq_length,d_model))
 
     return PE
     
+# print(positional_encoding(5, 4))
